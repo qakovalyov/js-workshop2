6c2f5ca48fcd23d12f0eea419577974c6242c3db
Victor Sych <victor.sych@gmail.com>
2014-07-03 19:50:14 +0300

    empty angular app with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular
+  .module('jsWorkshop2App', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
